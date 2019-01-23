c0637e5f7464826646548d7c50540d391b972180
SingularD <906229740@qq.com>
2019-01-23 23:20:42 +0800

    HomePage

--- src/container/Home.js
+++ src/container/Home.js
@@ -5,8 +5,24 @@ import '../App.css';
 import PriceList from '../components/PriceList'
 import ViewTab from '../components/ViewTab'
 import TotalPrice from '../components/TotalPrice'
-import {LIST_VIEW, CHART_VIEW} from "../utility";
+import { LIST_VIEW, CHART_VIEW, TYPE_INCOME, TYPE_OUTCOME, parseToYearAndMonth } from "../utility";
 import MonthPicker from '../components/MonthPicker'
+import CreateBtn from '../components/CreateBtn'
+
+const categories = {
+  "1": {
+    'id': '1',
+    'name': '旅游',
+    'type': 'outcome',
+    'iconName': 'ios-plane'
+  },
+  "2": {
+    'id': '2',
+    'name': '理财',
+    'type': 'income',
+    'iconName': 'ios-plane'
+  }
+}
 
 const items = [
   {
@@ -14,29 +30,49 @@ const items = [
     'title': '去广州旅游',
     'price': 200,
     'date': '2019-02-03',
-    'category': {
-      'id': '1',
-      'name': '旅游',
-      'type': 'outcome',
-      'iconName': 'ios-plane'
-    }
+    'cid': 1
   },
   {
     'id': 2,
     'title': '去广州旅游',
     'price': 200,
     'date': '2019-02-03',
-    'category': {
-      'id': '1',
-      'name': '旅游',
-      'type': 'outcome',
-      'iconName': 'ios-plane'
-    }
+    'cid': 1
+  },
+  {
+    'id': 3,
+    'title': '理财',
+    'price': 200,
+    'date': '2019-02-03',
+    'cid': 2
   }
 ]
 
 class Home extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      items,
+      currentDate: parseToYearAndMonth,
+      tabView: LIST_VIEW
+
+    }
+  }
   render() {
+    const { items, currentDate, tabVIew } = this.state
+    const itemWithCategory = items.map(item => {
+      item.category = categories[item.cid]
+      // 类似于数据库的外键
+      return item
+    })
+    let totalIncome = 0, totalOutcome = 0
+    itemWithCategory.forEach(item => {
+      if (item.category.type === TYPE_OUTCOME) {
+        totalOutcome += item.price
+      }else {
+        totalIncome += item.price
+      }
+    })
     return (
       <React.Fragment>
         <header className="App-header">
@@ -51,9 +87,25 @@ class Home extends React.Component{
                 onChange={() => {}}
               />
             </div>
-            <div className="col"></div>
+            <div className="col">
+              <TotalPrice
+                income={totalIncome}
+                outcome={totalOutcome}/>
+            </div>
           </div>
         </header>
+        <div className="content-area py-3 px-3">
+          <ViewTab
+            activeTab={LIST_VIEW}
+            onTabChange={() => {}}
+          />
+          <CreateBtn onClick={() => {}}/>
+          <PriceList
+            items={items}
+            onModifyItem={() => {}}
+            onDeleteItem={() => {}}
+          />
+        </div>
       </React.Fragment>
     )
   }
